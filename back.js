463ad907251bae805112c5ce45f367c0a6e2ee3a
vikhulei <vikhulei@users.noreply.github.com>
2020-09-18 21:20:48 +0300

    background color

--- back.js
+++ back.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
       $("#panel").animate({left: "+=100vw"}, 500, function(){
         $("header").css("visibility", "hidden"); 
         $("main").css("visibility", "hidden");
+        $("body").css("background-color", "white");
         $("#panel").animate({left: "+=100vw"}, 500, function(){
           window.location.href = "../index.html";
         })
