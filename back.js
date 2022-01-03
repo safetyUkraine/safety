3c024fcc43318b6d831ed1515c52e715037d8b35
viktor <vikhulei@gmail.com>
2022-01-03 03:52:43 +0200

    history.back replaced with href

--- back.js
+++ back.js
@@ -5,9 +5,11 @@ $(document).ready(function(){
         $("header, main").css("visibility", "hidden"); 
         $("body").css("background-color", "white");
         $("#panel").animate({left: "+=100vw"}, 500, function(){
-        window.history.back();
-        document.location.reload(true);
-        /* window.location.href = "../index.html"; */
+          // history.back();
+        // window.history.back();
+        // window.history.go(-1);
+        // document.location.reload(true);
+        window.location.href = "../index.html";
         })
       })
     })
